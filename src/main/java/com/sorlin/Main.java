b4df088cada24168ba80cde69b20d7135da00a1c
lance <875720292@qq.com>
2023-04-15 15:10:37 +0800

    feat: first commit

---
+++ src/main/java/com/sorlin/Main.java
@@ -0,0 +1,13 @@
+package com.sorlin;
+
+/**
+ * ${desc}
+ *
+ * @author lisongling 2023/4/15
+ * @since ${version}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
